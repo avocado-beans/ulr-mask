57cfa9501492a3a34f92799cb205b58fa8011465
anon <122219518+avocado-beans@users.noreply.github.com>
2024-07-10 19:45:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ app = FastAPI()
 async def confirm(request: Request):
     return "Your server is working!"
     
-@app.post("/gemini/")
-async def read_items(request: Request):
+@app.post("/image/")
+async def read_image(request: Request):
     
     with open("question.png", "wb") as image:
         image.write(base64.b64decode(request.query_params['image'].replace('data:image/png;base64,','')))
         
-    return gemini.answer(request.query_params['key'], request.query_params['prompt'], "question.png", request.query_params['model_name'])
+    return gemini.text_in_image(request.query_params['key'], request.query_params['prompt'], "question.png", request.query_params['model_name'])
+
+@app.post("/text/")
+async def read_text(request: Request):
+    
+    return gemini.text_only(request.query_params['key'], request.query_params['prompt'], request.query_params['model_name'])
+
 

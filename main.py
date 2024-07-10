0dec3dad427366ea1ea379f2a92b1fc713da00b2
anon <122219518+avocado-beans@users.noreply.github.com>
2024-07-10 19:34:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def confirm(request: Request):
 async def read_items(request: Request):
     
     with open("question.png", "wb") as image:
-        image.write(base64.b64decode(request.query_params['image']))
+        image.write(base64.b64decode(request.query_params['image'].replace('data:image/png;base64,','')))
         
     return gemini.answer(request.query_params['key'], request.query_params['prompt'], "question.png", request.query_params['model_name'])
 
